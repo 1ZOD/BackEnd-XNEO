f884c947e83dd8c31197f824dd66fa38fe548d69
1Zod <nathanmarrega@gmail.com>
2023-06-27 14:41:08 -0300

    fix:corringido-estrutura

---
+++ BackEnd/config/__db.py
@@ -0,0 +1,16 @@
+from flaskext.mysql import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def configure_mysql(app):
+    app.config['MYSQL_DATABASE_HOST'] = os.getenv("BD_HOST")
+    app.config['MYSQL_DATABASE_USER'] = os.getenv("BD_USER")
+    app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("BD_PASSWAOR")
+    app.config['MYSQL_DATABASE_DB'] = os.getenv("BD_TABLE")
+
+    mysql = MySQL()
+    mysql.init_app(app)
+    
+    return mysql
